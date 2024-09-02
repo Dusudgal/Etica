1dadb7382608d0284bcfc65a0a128c83b7d4d5a3
YUJEONG\kim94 <claudia@claudia.com>
2024-09-02 13:53:35 +0900

    feat : 회원가입 구현

---
+++ eticaPlanner/src/main/java/com/eticaplanner/eticaPlanner/user/service/UserService.java
@@ -0,0 +1,33 @@
+package com.eticaplanner.eticaPlanner.user.service;
+
+import com.eticaplanner.eticaPlanner.user.entity.UserEntity;
+import com.eticaplanner.eticaPlanner.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserEntity getUserEntityByUserId(String user_id){
+        return userRepository.findByUserId(user_id);
+    }
+
+    public Integer addUser(String loginId, String password, String name, String phone, String birth, String gender){
+       UserEntity userEntity = userRepository.save(
+               UserEntity.builder()
+                       .userId(loginId)
+                       .userPassword(password)
+                       .userName(name)
+                       .userPhone(phone)
+                       .userBirth(birth)
+                       .userGender(gender)
+                       .build()
+       );
+       return userEntity == null ? null : userEntity.getUserNo();
+    }
+
+//    public UserEntity getUserEntityByUserNo(String user_no){}
+}
