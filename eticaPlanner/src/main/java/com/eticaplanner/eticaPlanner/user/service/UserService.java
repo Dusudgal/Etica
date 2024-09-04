1528cc8fb9b3d1ab599ee6ec913123442bb6b039
ryumin10 <dbalsduf9797@naver.com>
2024-09-04 17:51:21 +0900

    feat : 마이페이지 수정전
    로그인폼 추가
    마이페이지 수정

--- eticaPlanner/src/main/java/com/eticaplanner/eticaPlanner/user/service/UserService.java
+++ eticaPlanner/src/main/java/com/eticaplanner/eticaPlanner/user/service/UserService.java
@@ -15,19 +15,21 @@ public class UserService {
         return userRepository.findByUserId(user_id);
     }
 
-//    public Integer addUser(String loginId, String password, String name, String phone, String birth, String gender){
-//       UserEntity userEntity = userRepository.save(
-//               UserEntity.builder()
-//                       .userId(loginId)
-//                       .userPassword(password)
-//                       .userName(name)
-//                       .userPhone(phone)
-//                       .userBirth(birth)
-//                       .userGender(gender)
-//                       .build()
-//       );
-//       return userEntity == null ? null : userEntity.getUserNo();
-//    }
+    public Integer addUser(String user_id, String user_password, String user_name, String user_phone, String user_birth, String user_gender){
+       UserEntity userEntity = userRepository.save(
+               UserEntity.builder()
+                       .userId(user_id)
+                       .userPassword(user_password)
+                       .userName(user_name)
+                       .userPhone(user_phone)
+                       .userBirth(user_birth)
+                       .userGender(user_gender)
+                       .build()
+       );
+       return userEntity == null ? null : userEntity.getUserNo();
+    }
 
-//    public UserEntity getUserEntityByUserNo(String user_no){}
+    public UserEntity getUserEntityByUserIdPassword(String user_id, String user_password){
+        return userRepository.findByUserIdAndUserPassword(user_id, user_password);
+    }
 }
--- eticaPlanner/src/main/webapp/WEB-INF/views/User/signIn.jsp
+++ eticaPlanner/src/main/webapp/WEB-INF/views/User/signIn.jsp
@@ -14,8 +14,8 @@
     <div class="login-wrapper">
         <h2>로그인</h2>
         <form id="loginForm" method="post" action="/user/sign-in">
-            <input type="text" name="userName" placeholder="아이디를 입력해주세요.">
-            <input type="password" name="userPassword" placeholder="비밀번호를 입력해주세요.">
+            <input type="text" name="user_id" placeholder="아이디를 입력해주세요.">
+            <input type="password" name="user_password" placeholder="비밀번호를 입력해주세요.">
             <label for="remember-check">
                 <input type="checkbox" id="remember-check">아이디 저장하기
                 <a href="/user/sign-up-view">회원가입 하러가기</a>
@@ -30,14 +30,14 @@
         $("#loginForm").on('submit', function (e){
             e.preventDefault();
             //alert("로그인");
-            let userName = $("input[name=userName]").val().trim();
-            let userPassword = $("input[name=userPassword]").val();
+            let user_id = $("input[name=user_id]").val().trim();
+            let user_password = $("input[name=user_password]").val();
 
-            if(!userName){
+            if(!user_id){
                 alert("아이디를 입력해주세요.");
                 return false;
             }
-            if(!userPassword){
+            if(!user_password){
                 alert("비밀번호를 입력해주세요.");
                 return false;
             }
@@ -58,7 +58,6 @@
                     alert(data.error_message);
                 }
             });
-            ;
         });
     })
 </script>
